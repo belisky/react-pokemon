1d533bc942e0d9f4693d6de68ab641403a44dacc
bel <denoblesnobility2@gmail.com>
2021-11-11 21:21:13 +0000

    still no avail to style pagination to center,im suspecting the bgimage to be the cause

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Pagination = ({gotoNextPage,gotoPrevPage}) => {
     return (
-        <div className="pagination container ">
+        <div className="container">
+            <div className="pagination">
             
-             
-                {gotoPrevPage &&<button className="btn" onClick={gotoPrevPage}>Prev</button>}
-                {gotoNextPage &&<button className="btn" onClick={gotoNextPage}>Next</button>}
-              
+            
+                    {gotoPrevPage &&<button className="btn" onClick={gotoPrevPage}>Prev</button>}
+                    {gotoNextPage &&<button className="btn" onClick={gotoNextPage}>Next</button>}
+            
+            </div>
         </div>
     )
 }
